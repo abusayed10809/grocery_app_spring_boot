769158dc51f5cd8a1d4a63873aa2020191727cb8
abdullah.sayed@technonext.com <abdullah.sayed@technonext.com>
2026-03-16 15:07:07 +0600

    db up and running

---
+++ src/main/java/com/tn/grocery_app/entity/UserEntity.java
@@ -0,0 +1,15 @@
+package com.tn.grocery_app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
